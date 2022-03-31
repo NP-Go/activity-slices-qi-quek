0abf956adddf98e7f5fce89c7f1932b4081d70df
narwhal04 <q.quek7@gmail.com>
2022-03-31 22:55:35 +0800

    functioning code

--- slices.go
+++ slices.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
+var (
+	sliceList = []string{"$9.50", "$8.00", "$10.20", "$7.40"}
+	// sliceList = make([]string, 5, 5)
+)
+
 func main() {
 	//Insert Code here
+	// sliceList[0] = "$9.50"
+	// sliceList[1] = "$8.00"
+	// sliceList[2] = "$10.20"
+	// sliceList[3] = "$7.40"
+
+	fmt.Println(len(sliceList))
+	fmt.Println(cap(sliceList))
+
+	fmt.Println(sliceList[3-1])
+	sliceList[3-1] = "9.80"
+	fmt.Println(sliceList[3-1])
+
+	sliceList = append(sliceList, "$8.40")
+	sliceList = append(sliceList, "9.40")
+	sliceList = append(sliceList, "$7.20")
+
+	fmt.Println(sliceList)
+	fmt.Println(len(sliceList))
+	fmt.Println(cap(sliceList))
+
+	sliceNew := sliceList[3-1:]
+	fmt.Println(len(sliceNew))
+	fmt.Println(cap(sliceNew))
+
 }
